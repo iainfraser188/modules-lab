7097f875fda972ead3085091f17dd3e10965b0c6
iain <iainfraser188@msn.com>
2021-10-14 14:40:01 +0100

    first commit

---
+++ output.py
@@ -0,0 +1,12 @@
+def print_task_descriptions(list):
+    for task in list:
+        print(task["description"])
+
+def print_list(list):
+    for task in list:
+        print_task(task)        
+
+def print_task(task):
+    print(f'Description: { task["description"] }')
+    print(f'Status: { "Completed" if task["completed"] else "Incomplete"}')
+    print(f'Time Taken: {task["time_taken"]} mins')        
